248413c460e3ef2e3976cc375db26fc21b98037d
Harshil Gupta <53968071+Harshil-Gupta@users.noreply.github.com>
2021-06-13 01:35:55 +0530

    Add files via upload

---
+++ seeds/index.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const Campground = require('../models/campground');
+const cities = require('./cities');
+const {places,descriptors} = require('./seedHelper');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console,"connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+const sample = (arr) => {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+const seedDB = async()=>{
+    await Campground.deleteMany({});
+    for(let i=0;i<50;i++){
+        const random1000 = Math.floor(Math.random()*1000);
+        const price = Math.floor(Math.random() * 20) + 10;
+        const camp = new Campground({
+            location: `${cities[random1000].city},${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem Ipsum is the single greatest threat. We are not - we are not keeping up with other websites. Lorem Ipsum best not make any more threats to your website. It will be met with fire and fury like the world has never seen. Does everybody know that pig named Lorem Ipsum? An ‘extremely credible source’ has called my office and told me that Barack Obama’s placeholder text is a fraud.',
+            price
+        })
+    await camp.save();
+    }
+}
+
+seedDB().then(() =>{
+    mongoose.connection.close();
+});
